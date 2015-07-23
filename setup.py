520693a9b0aad077305a4ca65f63f0ff05df4545
Brett Vitaz <brett@vitaz.net>
2015-07-23 11:52:43 -0700

    [setup.py] Add packages, keywords, fix url.

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from distutils.core import setup
 setup(
     name='crestroncleanup',
     version='0.1',
-    packages=[''],
-    url='https://github.com/brettvitaz/crestron_cleanup',
+    packages=['crestroncleanup'],
+    url='https://github.com/brettvitaz/crestroncleanup',
     license='nunya',
     author='brettvitaz',
     author_email='brett@vitaz.net',
-    description='Clean up signal names and other parts of a messy Crestron SIMPL program.'
+    description='Clean up signal names and other parts of a messy Crestron SIMPL program.',
+    keywords=['crestron', 'simpl', 'cleanup'],
+    classifiers=[]
 )
