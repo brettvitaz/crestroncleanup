6fb994120979cf856d3867b4ee93e739beb4c576
Brett Vitaz <brett@vitaz.net>
2015-07-23 20:49:03 -0700

    [console] Covert getopt to argparse for parsing args. - Move business logic to app.py

--- crestroncleanup/crestroncleanup_console.py
+++ crestroncleanup/crestroncleanup_console.py
@@ -1,73 +1,20 @@
-import collections
-import getopt
-import shutil
-import sys
-import time
-
-from crestroncleanup import obj_types
+import argparse
+from app import App
 
 
 def main():
-    start_time = time.time()
+    parser = argparse.ArgumentParser(description='Clean up signals in a messy Crestron SIMPL file.')
+    parser.add_argument('filename', help='Name of file to process')
+    parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite the existing file', default=False)
+    parser.add_argument('-b', '--backup', action='store_true', help='Backup existing file before writing', default=False)
 
-    filename = ''
-    new_ext = '.new'
-    old_ext = '.old'
-    newline = '\n'
-    overwrite = False
-    backup = True
+    args = parser.parse_args()
 
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'i:n:', ['overwrite'])
-    except getopt.GetoptError:
-        print('-i <filename>')
-        sys.exit(2)
-    else:
-        if not len(opts):
-            print('-i <filename>')
-            sys.exit(2)
+    filename = args.filename
+    overwrite = args.overwrite
+    backup = args.backup
 
-    for opt, arg in opts:
-        if opt == '-i':
-            filename = arg
-        elif opt == '-n':
-            newline = bytes(arg, 'utf8').decode('unicode-escape')
-        elif opt == '--overwrite':
-            overwrite = True
-            new_ext = ''
+    print(filename, overwrite, backup)
 
-    if overwrite and backup:
-        shutil.copy(filename, filename + old_ext)
-
-    obj_store = obj_types.ObjStore()
-    obj_data = collections.OrderedDict()
-    with open(filename) as file:
-        for line in file.readlines():
-            line_data = line.strip()
-            if line_data == '[':
-                obj_data = collections.OrderedDict()
-            elif line_data == ']':
-                obj_store.add_item(obj_data)
-            else:
-                try:
-                    k, v = line_data.split('=', 1)
-                except ValueError:
-                    print('Failed to parse line: {}'.format(line_data))
-                    sys.exit(2)
-
-                obj_data.update({k: v})
-
-    obj_store.process()
-
-    with open(filename + new_ext, mode='w') as file:
-        for obj in obj_store.items():
-            obj_lines = ['[{}'.format(newline)]
-            for k, v in obj:
-                obj_lines.append('{}={}{}'.format(k, v, newline))
-            obj_lines.append(']{}'.format(newline))
-
-            file.writelines(obj_lines)
-
-    end_time = time.time()
-
-    print('Elapsed time: {}'.format(end_time - start_time))
+    app = App(filename, overwrite, backup)
+    app.process()
