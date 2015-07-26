0b70600d55a913ff707a4ef0a8d364068d1ec07f
Brett Vitaz <brett@vitaz.net>
2015-07-26 13:58:16 -0700

    [Main] Move __main__ to crestroncleanup package

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from crestroncleanup import crestroncleanup_console
-
-if __name__ == '__main__':
-    crestroncleanup_console.main()
---
+++ crestroncleanup/__main__.py
@@ -0,0 +1,9 @@
+import sys
+from crestroncleanup import crestroncleanup_console
+import crestroncleanup_gui
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        crestroncleanup_console.main()
+    else:
+        crestroncleanup_gui.main()
