66e60ef58f09ef779242c0581d30fae1902471bc
Brett Vitaz <brett@vitaz.net>
2015-07-27 17:20:53 -0700

    [FileServices] Extra documentation.

--- crestroncleanup/file_services.py
+++ crestroncleanup/file_services.py
@@ -12,8 +12,15 @@ NEWLINE_NIX = '\r\n'
 
 
 def read_file(filename):
+    """
+    Read a file and return a object store, which will contain all of the program objects ready for display or processing.
+    :param filename: Full path to the file to process.
+    :return: ObjStore object containing all of the objects to be processed.
+    """
     obj_store = ObjStore()
     obj_data = collections.OrderedDict()
+
+    # Read and parse the file
     with open(filename) as f:
         for line in f.readlines():
             line_data = line.strip()
@@ -34,16 +41,26 @@ def read_file(filename):
 
 
 def save_file(obj_store, filename, overwrite=False, backup=True):
+    """
+    Write an output file from the ObjStore object.
+    :param obj_store: ObjStore object containing all of the processed objects.
+    :param filename: Full path to the output file.
+    :param overwrite: Specify True to overwrite an existing file, False to create a new file with an extension `.new`.
+    :param backup: Specify True to backup the existing file if overwriting, creating a copy with the extension `.old`.
+    """
+    # Windows writes `\n` as `\r\n`, so it is necessary to specify the delimiter here.
     if platform.system() == 'Windows':
         newline = NEWLINE_WIN
     else:
         newline = NEWLINE_NIX
 
+    # Backup the existing file if necessary.
     if overwrite and backup:
         shutil.copy(filename, filename + OLD_EXT)
 
     new_ext = NEW_EXT if overwrite is False else ''
 
+    # Write the file
     with open(filename + new_ext, mode='w') as f:
         for obj in obj_store.items():
             obj_lines = ['[{}'.format(newline)]
